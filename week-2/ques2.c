96a26f18bb3d2cc5c95caf711f21eb6136fd4c68
prik73 <prinovac@gmail.com>
2024-08-01 09:48:59 +0000

    week-2 ques2 added

---
+++ week-2/ques2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void readMatrix(int rows, int cols, int matrix[rows][cols]) {
+    printf("Enter the elements of the matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void transposeMatrix(int rows, int cols, int matrix[rows][cols], int transpose[cols][rows]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+}
+
+void printMatrix(int rows, int cols, int matrix[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int rows, cols;
+
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter the number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+    int transpose[cols][rows];
+
+    readMatrix(rows, cols, matrix);
+    transposeMatrix(rows, cols, matrix, transpose);
+
+    printf("The transpose of the matrix is:\n");
+    printMatrix(cols, rows, transpose);
+
+    return 0;
+}
